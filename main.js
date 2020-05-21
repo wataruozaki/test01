ffeeb89fa15936d13d8592fcb714dc9d673cff93
wataruozaki <65584515+wataruozaki@users.noreply.github.com>
2020-05-21 20:39:00 +0900

    スペースを消しました

--- main.js
+++ main.js
@@ -10,8 +10,6 @@
     { c: "まあ普通でしょ", a: "もっと精進します。" }
   ];
   let currentNum = 0;
-  
-
   const que = document.querySelector("#que");
   const modalText = document.querySelector("#modal-text");
   const modal = document.querySelector("#modal");
